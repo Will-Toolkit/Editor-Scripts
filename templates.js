ea277dee253a4d7390cd8ea9ef3536fca081f2be
Will-Toolkit <156904176+Will-Toolkit@users.noreply.github.com>
2024-01-26 11:59:23 +0000

    Create templates.js

---
+++ templates.js
@@ -0,0 +1,119 @@
+  /* TEMPLATES START */
+const templates = [
+    [
+        "Home header",
+        `<div class="row home-header imgFill" style="background-image: linear-gradient(), url();">
+  <div class="panel-inner">
+    <div class="col-xs-12 vertpad">
+
+    </div>
+  </div>
+</div>`
+    ],
+    [
+        "Row + col-xs-12",
+        `<div class="row">
+  <div class="panel-inner">
+    <div class="col-xs-12 vertpad">
+
+    </div>
+  </div>
+</div>`
+    ],
+    [
+        "col-sm-6's",
+        `<div class="row">
+  <div class="panel-inner equal-height">
+    <div class="col-xs-12 col-sm-6 vertpad align-self">
+      <h2></h2>
+      <p></p>
+    </div>
+    <div class="col-xs-12 col-sm-6 vertpad">
+      <img class="lazy objectFill" style="width: 100%;" data-src="">
+    </div>
+  </div>
+</div>`
+    ],
+    [
+        "Btn",
+        `<a class="btn primary" href="#">
+Lorem
+</a>`
+    ],
+    [
+        "½ panel left",
+        `<div class="row">
+  <div class="panel-inner equal-height">
+    <div class="col-xs-12 col-sm-6 vertpad sm-middle">
+      <div class="half-panel-left">
+        <h2></h2>
+        <p></p>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 nopad">
+      <img class="lazy objectFill" style="width: 100%;" data-src="">
+    </div>
+  </div>
+</div>`
+    ],
+    [
+        "½ panel right",
+        `<div class="row">
+  <div class="panel-inner equal-height">
+    <div class="col-xs-12 col-sm-6 col-sm-push-6 vertpad sm-middle">
+      <div class="half-panel-left">
+        <h2></h2>
+        <p></p>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-sm-pull-6 nopad">
+      <img class="lazy objectFill" style="width: 100%;" data-src="">
+    </div>
+  </div>
+</div>`
+    ],
+    [
+        "Expanding Container",
+        `<div class="expand-container">
+  <div class="expand-btn xs-middle">
+    <h4>
+      Question
+    </h4>
+    <i class="fa-solid fa-chevron-down">
+    </i>
+  </div>
+  <div class="minimize xs-middle">
+    <h4>
+      Question
+    </h4>
+    <i class="fa-solid fa-chevron-up">
+    </i>
+  </div>
+  <div class="expand-content">
+    <p>
+      Lorem Ipsum Dolor Sit Amet
+    </p>
+  </div>
+</div>`
+    ],
+    [
+        "Page anchor",
+        `<div class="pageanchor" id=""></div>`
+    ],
+    [
+        "Scroller",
+        `<tw_scroller sid="XXXX" name="SCROLLERNAME"></tw_scroller>`
+    ],
+    [
+        "Form",
+        `<iframe title="Form title" class="resizeMe" onload="setIframesize()" src="...form url/plain" width="100%" frameborder="0" scrolling="no"></iframe>`
+    ],
+    [
+        "Widget",
+        `<tw_widget wid="XXX"></tw_widget>`
+    ]
+
+
+
+];
+  /* TEMPLATES END */
